fa2f789fd69166858e4df33bee484217bb6e27dd
Abhinav Abhinav <abhinav.abhinav@realcore.de>
2025-06-19 17:36:46 +0200

    designing authentication page UI

--- sample-paper-app/src/pages/Authentication/Authentication.tsx
+++ sample-paper-app/src/pages/Authentication/Authentication.tsx
@@ -1,5 +1,29 @@
+import { Button, Flex } from "@chakra-ui/react"
+
 export const Authentication = () => {
   return (
-    <div>Welcome to authentication page</div>
+    <Flex
+      mx={"auto"}
+      id="authentication"
+      flexDirection={"column"}
+      justifyContent={"center"}
+      w={["90%", "90%", "60%", "50%", "50%"]}
+    >
+      <Button 
+        mb={3}
+        bg={"#3b82f6d6"}
+        fontWeight={"bold"}
+        fontSize={["xl", "xl", "1xl", "2xl", "2xl"]}
+      >
+        LOGIN WITH EMAIL
+      </Button>
+      <Button
+        bg={"#3b82f6d6"}
+        fontWeight={"bold"}
+        fontSize={["xl", "xl", "1xl", "2xl", "2xl"]}
+      >
+        REGISTER WITH EMAIL
+      </Button>
+    </Flex>
   )
 }
