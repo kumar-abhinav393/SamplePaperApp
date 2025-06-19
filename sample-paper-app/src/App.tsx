52fd08b363294545a8aefd8c4f6385941c60913a
Abhinav Abhinav <abhinav.abhinav@realcore.de>
2025-06-19 16:22:42 +0200

    Added the fix UI template for all app routers

--- sample-paper-app/src/App.tsx
+++ sample-paper-app/src/App.tsx
@@ -6,37 +6,46 @@ import { Login } from "./pages/Login/Login"
 import { Signup } from "./pages/Signup/Signup"
 import { FilterAssignments } from "./pages/FilterAssignments/FilterAssignments"
 import { MyAssignments } from "./pages/MyAssignments/MyAssignments"
+import { Flex, Grid } from "@chakra-ui/react"
+import { Header } from "./components/Header/Header"
 
 function App() {
   return (
     <Provider>
       <BrowserRouter>
-        <Routes>
-          <Route
-            path = {RouterPaths.Root}
-            element = {<Authentication/>}
-          />
-          <Route 
-            path = {RouterPaths.Signup}
-            element = {<Signup/>} 
-          />
-          <Route
-            path = {RouterPaths.Login}
-            element = {<Login/>}
-          />
-          <Route
-            path = {RouterPaths.FilterAssignments}
-            element = {<FilterAssignments/>} 
-          />
-          <Route 
-            path={RouterPaths.MyAssignments}
-            element = {<MyAssignments/>}
-          />
-          <Route
-            path={"*"}
-            element = {<Navigate to={RouterPaths.Root}/>}
-          />
-        </Routes>
+        <Flex justifyContent={"center"} alignItems={"center"} w={"100vw"} m={"auto"}>
+          <Grid height={"100dvh"} gridTemplateRows={"auto 1fr"} 
+            w={["100vw", "100vw", "100vw", "100vw", "90vw"]}
+            >
+            <Header/>
+            <Routes>
+              <Route
+                path = {RouterPaths.Root}
+                element = {<Authentication/>}
+              />
+              <Route 
+                path = {RouterPaths.Signup}
+                element = {<Signup/>} 
+              />
+              <Route
+                path = {RouterPaths.Login}
+                element = {<Login/>}
+              />
+              <Route
+                path = {RouterPaths.FilterAssignments}
+                element = {<FilterAssignments/>} 
+              />
+              <Route 
+                path={RouterPaths.MyAssignments}
+                element = {<MyAssignments/>}
+              />
+              <Route
+                path={"*"}
+                element = {<Navigate to={RouterPaths.Root}/>}
+              />
+            </Routes>
+          </Grid>
+        </Flex>
       </BrowserRouter>
     </Provider>
   )
