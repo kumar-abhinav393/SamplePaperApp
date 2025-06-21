4bda66c346954e9eb8ad81dd7ac38e7af78f23a2
Abhinav Abhinav <abhinav.abhinav@realcore.de>
2025-06-21 09:20:18 +0200

    designed login ui

--- sample-paper-app/src/pages/Login/Login.tsx
+++ sample-paper-app/src/pages/Login/Login.tsx
@@ -1,5 +1,103 @@
+import { Box, Button, Flex, Input, Text } from "@chakra-ui/react"
+
 export const Login = () => {
   return (
-    <div>Welcome to Login page</div>
+    <Flex id="login"
+      mx={"auto"}
+      alignItems={"center"}
+      flexDirection={"column"}
+      justifyContent={"center"}
+    >
+      <Flex
+        w={"100%"}
+        alignItems={"center"}
+        flexDirection={"column"}
+      >
+        <Text
+          fontWeight={"bold"}
+          fontSize={["2xl", "2xl", "3xl", "4xl", "4xl"]}
+        >
+          Login
+        </Text>
+        <Flex
+          mt={2}
+          gap={2}
+          alignItems={"center"}
+          fontSize={["l", "l", "1xl", "2xl", "2xl"]}
+        >
+          <Text>No account yet?</Text>
+          <Box>
+            <Text
+              cursor={"pointer"}
+              fontWeight={"bold"}
+              color={"#3b82f6d6"}
+            >
+              Signup
+            </Text>
+          </Box>
+        </Flex>
+        <Flex
+          gap={2}
+          alignItems={"center"}
+          fontSize={["l", "l", "1xl", "2xl", "2xl"]}
+        >
+          <Text>Forgot Password?</Text>
+          <Box>
+            <Text
+              cursor={"pointer"}
+              fontWeight={"bold"}
+              color={"#3b82f6d6"}
+            >
+              Change Password
+            </Text>
+          </Box>
+        </Flex>
+        <Flex
+          mt={4}
+          gap={2}
+          flexDirection={"column"}
+          alignItems={"flex-start"}
+          w={["310px", "350px", "400px", "450px"]}
+          fontSize={["l", "l", "xl", "1xl", "1xl"]}
+        >
+          <Text>EMAIL</Text>
+          <Input
+            variant={"outline"}
+            placeholder="user@domain.com"
+            css={{ "--focus-color": "#3b82f6d6" }}
+            fontSize={["xl", "xl", "1xl", "2xl", "2xl"]}
+          />
+          <Text mt={2}>PASSWORD</Text>
+          <Input
+            variant={"outline"}
+            placeholder="password >= 8"
+            css={{ "--focus-color": "#3b82f6d6" }}
+            fontSize={["xl", "xl", "1xl", "2xl", "2xl"]}
+          />
+        </Flex>
+        <Flex
+          mt={4}
+          w={"100%"}
+          justifyContent={"space-between"}
+        >
+          <Button
+            color={"white"}
+            bg={"#3b82f6d6"}
+            fontWeight={"bold"}
+            fontSize={["xl", "xl", "1xl", "2xl", "2xl"]}
+          >
+            Cancel
+          </Button>
+          <Button
+            color={"white"}
+            bg={"#3b82f6d6"}
+            fontWeight={"bold"}
+            fontSize={["xl", "xl", "1xl", "2xl", "2xl"]}
+          >
+            Login
+          </Button>
+        </Flex>
+      </Flex>
+    </Flex>
   )
 }
