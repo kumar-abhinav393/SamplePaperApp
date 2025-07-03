38a27083b12edbdac163a3e50ba4458f0b1db552
Abhinav Abhinav <abhinav.abhinav@realcore.de>
2025-07-03 16:00:33 +0200

    Create a separate component to filter the assignments wrt months

---
+++ sample-paper-app/src/components/TimeFilterSelect/TimeFilterSelect.tsx
@@ -0,0 +1,49 @@
+import { createListCollection, Flex, Portal, Select } from "@chakra-ui/react"
+
+export const TimeFilterSelect = () => {
+  const filterFrameworks = createListCollection({
+    items: [
+      { label: "This Month", value: "thismonth" },
+      { label: "Last Month", value: "lastmonth" },
+      { label: "Upcoming", value: "upcoming" },
+      { label: "All", value: "all" },
+    ],
+  })
+  return (
+    <Flex display={{ base: "block", lg: "none" }} w={"50%"}>
+      <Select.Root
+        collection={filterFrameworks}
+        size={["xs", "xs", "md"]}
+        deselectable>
+        <Select.HiddenSelect />
+        <Select.Control border={"1px solid #444746"} borderRadius={4}>
+          <Select.Trigger _focus={{ borderColor: "#3b82f6d6" }}>
+            <Select.ValueText
+              placeholder="Select Filter"
+              fontSize={["sm", "sm", "xl"]}
+            />
+          </Select.Trigger>
+          <Select.IndicatorGroup>
+            <Select.Indicator />
+          </Select.IndicatorGroup>
+        </Select.Control>
+        <Portal>
+          <Select.Positioner>
+            <Select.Content>
+              {filterFrameworks.items.map((framework) => (
+                <Select.Item
+                  item={framework}
+                  key={framework.value}
+                  fontSize={["sm", "sm", "xl"]}
+                >
+                  {framework.label}
+                  <Select.ItemIndicator />
+                </Select.Item>
+              ))}
+            </Select.Content>
+          </Select.Positioner>
+        </Portal>
+      </Select.Root>
+    </Flex>
+  )
+}
