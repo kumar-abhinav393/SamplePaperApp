696474d2109dabcbbb0ffeb92a7bbe9bbb935240
Abhinav Abhinav <abhinav.abhinav@realcore.de>
2025-07-06 12:53:47 +0200

    Integrated authContext with the app

--- sample-paper-app/src/main.tsx
+++ sample-paper-app/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { Provider } from './components/ui/provider.tsx'
+import { AuthContextProvider } from './context/authContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider>
-      <App />
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
     </Provider>
   </StrictMode>,
 )
