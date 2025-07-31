c213238d85a3f3c5fc418c6b3f6080fa1b5afb0e
Abhinav Abhinav <abhinav.abhinav@realcore.de>
2025-07-31 16:10:25 +0200

    Integrated the google login logic for mobile and larger screens

--- sample-paper-app/src/hooks/useGoogleAuthentication.ts
+++ sample-paper-app/src/hooks/useGoogleAuthentication.ts
@@ -1,28 +1,49 @@
-import { browserSessionPersistence, GoogleAuthProvider, setPersistence, signInWithPopup, type User } from "firebase/auth";
+import { browserSessionPersistence, getRedirectResult, GoogleAuthProvider, setPersistence, signInWithPopup, signInWithRedirect, type User } from "firebase/auth";
 import { useAuthContext } from "./useAuthContext";
 import { auth } from "../../firebase.config";
 import { createUserDocument } from "@/helpers/createUserDocument";
+import { useBreakpointValue } from "@chakra-ui/react";
+import { useEffect } from "react";
 
 export const useGoogleAuthentication = () => {
-
     const { dispatch } = useAuthContext()
+
+    const preferPopup = useBreakpointValue({ base: false, md: true }) ?? false;
+
+    const userDocument = async(user: User) => {
+        await createUserDocument(user);
+        dispatch({ type: "LOGIN", payload: user })
+    }
 
     const googleAuth = async() => {
         const provider = new GoogleAuthProvider()
 
         dispatch({ type: "IS_PENDING" })
-        
         try{
             await setPersistence(auth, browserSessionPersistence)
-            const { user } = await signInWithPopup(auth, provider)
-
-            await createUserDocument(user as User)
-
-            dispatch({ type: "LOGIN", payload: user as User })
+            if(preferPopup) {
+                const { user } = await signInWithPopup(auth, provider)
+                await userDocument(user)
+            } else {
+                await signInWithRedirect(auth, provider)
+            }
         } catch(error) {
             console.log(error as Error)
         }
     }
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const result = await getRedirectResult(auth);
+                if(result?.user) {
+                    await userDocument(result.user);
+                }
+            } catch(error) {
+                console.log(error as Error)
+            }
+        })();
+    });
 
     return { googleAuth }
 }
