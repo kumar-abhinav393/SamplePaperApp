8d4a754985da81dc4a78cbf5790413a912649fd7
Abhinav Abhinav <abhinav.abhinav@realcore.de>
2025-08-02 04:38:51 +0200

    changed the name of the firestore collection and added one more attribute in it

--- sample-paper-app/src/helpers/createUserDocument.ts
+++ sample-paper-app/src/helpers/createUserDocument.ts
@@ -1,9 +1,9 @@
 import type { User } from "firebase/auth";
-import { collection, doc, getDoc, setDoc } from "firebase/firestore";
+import { collection, doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
 import { db } from "../../firebase.config";
 
 export async function createUserDocument (user: User) {
-    const colRef = collection(db, "Students")
+    const colRef = collection(db, "students")
     const docRef = doc(colRef, user.uid)
     const docSnap = await getDoc(docRef)
     if(!docSnap.exists()) {
@@ -12,8 +12,9 @@ export async function createUserDocument (user: User) {
             email: user.email,
             displayName: user.displayName,
             role: "student",
+            createdAt: serverTimestamp(),
         }
-
+        
         await setDoc(docRef, data)
         const response = await getDoc(docRef)
         console.log("created doc: ", response.data())
