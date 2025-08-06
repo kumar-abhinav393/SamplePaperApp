6413d3fdcd24698423794739fd2a2d6057d902ba
Abhinav Abhinav <abhinav.abhinav@realcore.de>
2025-08-06 17:23:55 +0200

    Added noew interfaces for different collection props

--- sample-paper-app/src/types/types.ts
+++ sample-paper-app/src/types/types.ts
@@ -2,36 +2,68 @@ import type { User } from "firebase/auth";
 import type React from "react";
 
 interface AuthState {
-    user: User | null;
-    authIsReady: boolean;
-    pending: boolean;
-    dispatch: React.Dispatch<AuthAction>;
+  pending: boolean;
+  user: User | null;
+  authIsReady: boolean;
+
+  dispatch: React.Dispatch<AuthAction>;
 }
 
 type AuthAction =
-    | { type: "LOGIN"; payload: User }
-    | { type: "LOGOUT" }
-    | { type: "AUTH_IS_READY"; payload: User | null }
-    | { type: "IS_PENDING" }
+  | { type: "LOGIN"; payload: User }
+  | { type: "LOGOUT" }
+  | { type: "AUTH_IS_READY"; payload: User | null }
+  | { type: "IS_PENDING" };
 
 interface FirestoreState {
-    isPending: boolean;
-    error: string | null;
-    success: boolean;
+  isPending: boolean;
+  error: string | null;
+  success: boolean;
 }
 
 type FirestoreAction =
-    | { type: "IS_PENDING" }
-    | { type: "SUCCESS" }
-    | { type: "ERROR"; payload: string }
+  | { type: "IS_PENDING" }
+  | { type: "SUCCESS" }
+  | { type: "ERROR"; payload: string };
 
 interface ClassProps {
-    id: string;
-    props: {
-        active: boolean;
-        code: string;
-        name: string;
-    };
+  id: string;
+  props: {
+    active: boolean;
+    code: string;
+    name: string;
+  };
 }
 
-export type { AuthState, AuthAction, FirestoreState, FirestoreAction, ClassProps }
+interface SubjectProps {
+  id: string;
+  props: {
+    active: boolean;
+    boardFilters: string[];
+    classLevels: number[];
+    code: string;
+    name: string;
+  };
+}
+
+interface BoardProps {
+  id: string;
+  props: {
+    code: string;
+    name: string;
+    active: boolean;
+    country: string;
+    shortName: string;
+    classLevels: number[];
+  };
+}
+
+export type {
+  AuthState,
+  AuthAction,
+  FirestoreState,
+  FirestoreAction,
+  ClassProps,
+  SubjectProps,
+  BoardProps,
+};
