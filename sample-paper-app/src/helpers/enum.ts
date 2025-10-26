fc197423cd610c431ec1086e6bf70508193290ea
Abhinav Abhinav <abhinav.abhinav@realcore.de>
2025-10-26 10:50:14 +0100

    created enum to replace some hard coded values

---
+++ sample-paper-app/src/helpers/enum.ts
@@ -0,0 +1,18 @@
+enum TimeFilter {
+  All = "all",
+  Upcoming = "upcoming",
+  LastMonth = "lastMonth",
+  ThisMonth = "thisMonth",
+}
+
+enum SortOrder {
+    asc = "asc",
+    desc = "desc",
+}
+
+enum ColorMode {
+    black = "black",
+    white = "white",
+} 
+
+export {TimeFilter, SortOrder, ColorMode}
