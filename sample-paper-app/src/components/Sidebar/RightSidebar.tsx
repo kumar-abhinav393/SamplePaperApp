898e08ccc7a8dbb5a4113f7652480f0ebc416403
Abhinav Abhinav <abhinav.abhinav@realcore.de>
2025-11-12 08:11:53 +0100

    removed unnecessary code

--- sample-paper-app/src/components/Sidebar/RightSidebar.tsx
+++ sample-paper-app/src/components/Sidebar/RightSidebar.tsx
@@ -94,7 +94,7 @@ export const RightSidebar = ({
                   colorScheme="blue"
                   fontSize={["l", "xl", "1xl", "1xl", "1xl"]}
                 >
-                  
+
                 </Tag.Label>
               </Tag.Root>
             </Flex>
@@ -134,57 +134,6 @@ export const RightSidebar = ({
             </Flex>
           </>
         )}
-        <Flex gap={3} flexDirection="column" alignItems="center">
-          <Text
-            color={"#3bc8f6d6"}
-            fontWeight="medium"
-            fontSize={["l", "xl", "1xl", "1xl", "1xl"]}
-          >
-            Total Subjects
-          </Text>
-          <Tag.Root bg={textColor} border={"1px solid black"}>
-            <Tag.Label
-              colorScheme="blue"
-              fontSize={["l", "xl", "1xl", "1xl", "1xl"]}
-            >
-              {totalSubjects}
-            </Tag.Label>
-          </Tag.Root>
-        </Flex>
-        <Flex gap={3} flexDirection="column" alignItems="center">
-          <Text
-            color={"#3bc8f6d6"}
-            fontWeight="medium"
-            fontSize={["l", "xl", "1xl", "1xl", "1xl"]}
-          >
-            My Downloads
-          </Text>
-          <Tag.Root bg={textColor} border={"1px solid black"}>
-            <Tag.Label
-              colorScheme="blue"
-              fontSize={["l", "xl", "1xl", "1xl", "1xl"]}
-            >
-              0
-            </Tag.Label>
-          </Tag.Root>
-        </Flex>
-        <Flex gap={3} flexDirection="column" alignItems="center">
-          <Text
-            color={"#3bc8f6d6"}
-            fontWeight="medium"
-            fontSize={["l", "xl", "1xl", "1xl", "1xl"]}
-          >
-            My Notifications
-          </Text>
-          <Tag.Root bg={textColor} border={"1px solid black"}>
-            <Tag.Label
-              colorScheme="green"
-              fontSize={["l", "xl", "1xl", "1xl", "1xl"]}
-            >
-              0
-            </Tag.Label>
-          </Tag.Root>
-        </Flex>
       </Stack>
     </>
   );
