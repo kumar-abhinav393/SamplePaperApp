9a6fc44efdcdb07ff6f8a27a78fa5a6ceaad394d
Abhinav Abhinav <abhinav.abhinav@realcore.de>
2025-11-22 07:20:29 +0100

    Created the user doc wrt the roles (student, faculty, and admin)

--- sample-paper-app/src/helpers/createUserDocument.ts
+++ sample-paper-app/src/helpers/createUserDocument.ts
@@ -1,22 +1,39 @@
 import type { User } from "firebase/auth";
-import { collection, doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
+import { collection, doc, getDoc, serverTimestamp, setDoc, updateDoc } from "firebase/firestore";
 import { db } from "../../firebase.config";
+import { validateInviteToken } from "./validateInviteToken";
+import { UserRole } from "./enum";
 
-export async function createUserDocument (user: User) {
+export async function createUserDocument(user: User) {
     const colRef = collection(db, "users")
     const docRef = doc(colRef, user.uid)
     const docSnap = await getDoc(docRef)
-    if(!docSnap.exists()) {
+    if (!docSnap.exists()) {
+        const inviteToken = localStorage.getItem("inviteToken");
+        let role = UserRole.STUDENT;
+
+        if (inviteToken) {
+            const inviteData = await validateInviteToken(inviteToken);
+            role = inviteData.role;
+            const inviteRef = doc(db, "Invites", inviteToken);
+            await updateDoc(inviteRef, {
+                usedBy: user.uid,
+            });
+
+            localStorage.removeItem("inviteToken");
+        }
+
         const data = {
             uid: user.uid,
             email: user.email,
             displayName: user.displayName,
-            role: "student",
+            role: role,
             createdAt: serverTimestamp(),
         }
-        
-        await setDoc(docRef, data)
-        const response = await getDoc(docRef)
-        console.log("created doc: ", response.data())
+
+        await setDoc(docRef, data);
+
+        const response = await getDoc(docRef);
+        console.log("created doc: ", response.data());
     }
 }
