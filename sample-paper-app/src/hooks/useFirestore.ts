8bf1eabce0e722a780528414032cf80dea9dc3a8
Abhinav Abhinav <abhinav.abhinav@realcore.de>
2026-01-21 11:37:31 +0100

    Created the firestore hook to delete the document of the current user

---
+++ sample-paper-app/src/hooks/useFirestore.ts
@@ -0,0 +1,51 @@
+import { firestoreReducer } from "@/reducers/firestoreReducer"
+import type { FirestoreAction, FirestoreState } from "@/types/types"
+import { collection, deleteDoc, doc, type DocumentData} from "firebase/firestore"
+import { useEffect, useReducer, useState } from "react"
+import { db } from "../../firebase.config"
+import { deleteObject, getStorage, ref } from "firebase/storage"
+
+const initialState: FirestoreState = {
+    isPending: false,
+    error: null,
+    success: false,
+}
+
+export const useFirestore = <DocumentType extends DocumentData>(collectionId: string) => {
+    const [response, dispatch] = useReducer(firestoreReducer, initialState);
+    const [isCanceled, setIsCanceled] = useState(false);
+
+    console.log("CollectionId: ", collectionId);
+    const colRef = collection(db, collectionId);
+    console.log("colRef: ", colRef.id)
+    const dispatchIfNotCanceled = (action: FirestoreAction) => {
+        if (!isCanceled) {
+            dispatch(action);
+        }
+    }
+    const deleteDocument = async (documentId: string, filePath: string, onSuccess?: () => void) => {
+        try {
+            console.log("DocumentId: ", documentId);
+            dispatch({type: "IS_PENDING"});
+            const docRef = doc(colRef, documentId);
+            await deleteDoc(docRef);
+            if(filePath) {
+                const storage = getStorage();
+                const fileRef = ref(storage, filePath);
+                await deleteObject(fileRef);
+                console.log("Deleted PDF file.");
+            }
+            onSuccess?.();
+            dispatchIfNotCanceled({type: "SUCCESS"});
+        } catch (error) {
+            console.log("Delete failed: ", error);
+            dispatchIfNotCanceled({type: "ERROR", payload: "Could not delete the document or file."});
+        }
+    };
+
+    useEffect(() => {
+        return () => setIsCanceled(true)
+    }, []);
+
+    return { deleteDocument, response };
+}
